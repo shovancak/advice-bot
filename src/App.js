261f172c65e684113d122185531c0590ee7c67a3
shovancak <s.hovancak@gmail.com>
2020-07-21 18:31:26 +0200

    working functionality with styles issues

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+import Icon from "./images/bot-icon.png";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="background container">
-      <div>ROW 1</div>
-      <div>ROW 2</div>
-    </div>
-  );
+class App extends Component {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get(`https://api.adviceslip.com/advice`)
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice: advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="heading">
+          <div className="bot">
+            <img src={Icon} alt="bot-icon" width="110" />
+          </div>
+          <div className="title">
+            <h1>
+              Nice to see you. I'am Advice-Bot created to give best advices. My
+              friends call me Oliver.
+            </h1>
+          </div>
+        </div>
+        <div className="card border">
+          <h1 className="advice">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>Give Advice</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Advice/Advice.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Advice() {
-  return <div>ADVICE</div>;
-}
